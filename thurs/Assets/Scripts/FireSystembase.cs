a2aa1b25aceb8eeba80d49853b3687ec3d4ece07
wf122332 <76959987+wf122332@users.noreply.github.com>
2022-11-02 18:08:03 +0800

    update:新增發射系統

---
+++ thurs/Assets/Scripts/FireSystembase.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSystembase : MonoBehaviour
+{
+    [SerializeField, Header("子彈預製物")]
+    private GameObject prefabBullet;
+    [SerializeField, Header("子彈生成點")]
+    private Transform pointSpawn;
+
+
+}
---
+++ thurs/Assets/Scripts/MoveSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace S
+{
+    public class MoveSystem : MonoBehaviour
+    {
+        [SerializeField, Header("移動速度"), Range(-10, 0)]
+        private float speed = -3.5f;
+
+        private void Update()
+        {
+            transform.Translate(speed*Time.deltaTime, 0, 0);
+        }
+
+
+
+    }
+
+}
